f39d7c369b974e50fcf888cda814526bd41f51da
we-expand <cleber.couto@we-expand.com>
2026-04-21 00:22:32 -0300

    fix: correct index.css path in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App'
-import './index.css'
+import './app/index.css'
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
