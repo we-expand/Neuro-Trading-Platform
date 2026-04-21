0245d8ccf54edd376ef3b39afdccc122ea27785b
we-expand <cleber.couto@we-expand.com>
2026-04-20 21:42:53 -0300

    fix: restore full sidebar menus and sanitize figma assets

--- dist/assets/DevLab-CD0F9tsv.js
+++ dist/assets/DevLab-CD0F9tsv.js
@@ -1,4 +1,4 @@
-import{j as e}from"./index-D9ZLJSx0.js";import{c as D,R as S,r as v,m as p,B as P,P as L,F as R,a as M,b as F,d as _,C as B,T as C,e as z,X as $,Y as X,f as W,g as q,h as T,i as Z,j as V,L as K,S as J,k as Y,Z as Q}from"./App-kY0IBFJ-.js";/**
+import{j as e}from"./index-l_umye5O.js";import{c as D,R as S,r as v,m as p,B as P,P as L,F as R,a as M,b as F,d as _,C as B,T as C,e as z,X as $,Y as X,f as W,g as q,h as T,i as Z,j as V,L as K,S as J,k as Y,Z as Q}from"./App-BtipyuNP.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
