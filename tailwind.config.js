625c8b1df44d8c229ef5e7540e3a608d229e33c4
we-expand <cleber.couto@we-expand.com>
2026-04-21 14:17:26 -0300

    fix: corrige Tailwind e reset CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export default {content:["./index.html","./src/**/*.{js,jsx,ts,tsx}"],theme:{extend:{}},plugins:[]}
